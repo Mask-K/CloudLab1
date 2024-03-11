c6ced55ad4ac44e238045a76a104ecccf466d620
Maksym Klymenko <maksym@pop-os.localdomain>
2024-03-11 18:06:21 +0200

    added localize_objects

--- apiCalls.py
+++ apiCalls.py
@@ -1,3 +1,8 @@
+from io import BytesIO
+
+from google.cloud import vision
+from PIL import Image, ImageDraw, ImageFont
+
 import os
 
 import config
@@ -6,8 +11,6 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config.API_FILE
 
 
 def detect_labels(content: bytes):
-    from google.cloud import vision
-
     client = vision.ImageAnnotatorClient()
 
     image = vision.Image(content=content)
@@ -24,3 +27,35 @@ def detect_labels(content: bytes):
         )
 
     return labels_str
+
+
+def localize_objects(content: bytes):
+    client = vision.ImageAnnotatorClient()
+
+    image = vision.Image(content=content)
+    objects = client.object_localization(image=image).localized_object_annotations
+
+    image_bytes = BytesIO(content)
+    pil_image = Image.open(image_bytes)
+    draw = ImageDraw.Draw(pil_image)
+
+    font = ImageFont.load_default(size=10)
+
+    for object_ in objects:
+        vertices = [(vertex.x * pil_image.width, vertex.y * pil_image.height) for vertex in
+                    object_.bounding_poly.normalized_vertices]
+        draw.polygon(vertices, outline="red")
+
+        text_x, text_y = vertices[0]
+
+        name_text = f"{object_.name}"
+        percentage_text = f"{object_.score * 100:.2f}%"
+        label_text = f"{name_text} ({percentage_text})"
+
+        draw.text((text_x, text_y), label_text, font=font, fill="red")
+
+    image_bytes = BytesIO()
+    pil_image.save(image_bytes, format='PNG')
+    image_bytes.seek(0)
+    return image_bytes
+
--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# from apiCalls import detect_labels
-#
-# with open("img6.jpeg", "rb") as image_file:
-#     print(detect_labels(image_file.read()))
-
 
 import asyncio
 import logging
@@ -10,10 +5,11 @@ import sys
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, BufferedInputFile
 
 import config
-from apiCalls import detect_labels
+from apiCalls import detect_labels, localize_objects
+
 
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher()
@@ -26,45 +22,38 @@ async def cmd_start(message: Message):
 
 @dp.message(F.photo)
 async def photo_handler(message: Message):
-    # photo_data = message.photo[-1]
-    # await message.answer(f'{photo_data}')
+
     photo_data = message.photo[-1]
 
-    # Get the file ID of the photo
     file_id = photo_data.file_id
 
-    # Use the bot.get_file method to get information about the file
     file_info = await bot.get_file(file_id)
 
-    # Download the file using file_path from file_info
     file_path = file_info.file_path
     file_bytes_io = await bot.download_file(file_path)
     file_bytes_io.seek(0)
     file_bytes = file_bytes_io.read()
+
+    annotated_image_bytes_io = localize_objects(file_bytes)
+    annotated_image_bytes = annotated_image_bytes_io.getvalue()
+    annotated_image_file = BufferedInputFile(annotated_image_bytes, filename="annotated_image.png")
+    await message.answer_photo(annotated_image_file)
+
     await message.answer(detect_labels(file_bytes))
 
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
-        # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
 
 async def main():
+    await bot.delete_webhook()
     await dp.start_polling(bot)
 
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 asyncio.run(main())
-
-# with open("img6.jpeg", "rb") as image_file:
-#     print(image_file.read())
